b4cc07019ce2c785d52663a65cc6416fed6db477
Daniel Keva <danielkeva@Daniels-MacBook-Pro.local>
2020-06-30 11:22:15 +0300

    Updates

---
+++ precache-manifest.9fe96e9858e28c4f9d381af96ecf31c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa37aee43e76edc213f3c63be721c274",
+    "url": "/fintek-digital/index.html"
+  },
+  {
+    "revision": "a25743685aba1ad8e7e6",
+    "url": "/fintek-digital/static/css/main.6a06e934.chunk.css"
+  },
+  {
+    "revision": "71710cd46ce488b151b7",
+    "url": "/fintek-digital/static/js/2.3fe500ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fintek-digital/static/js/2.3fe500ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25743685aba1ad8e7e6",
+    "url": "/fintek-digital/static/js/main.9d7cd38b.chunk.js"
+  },
+  {
+    "revision": "75a93a4f0298a4a51659",
+    "url": "/fintek-digital/static/js/runtime-main.774b1e0d.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/fintek-digital/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/fintek-digital/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/fintek-digital/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
